e6c651e43e43e6da49676c0588c507c758f053e0
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-03-17 08:22:46 -0400

    some

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,28 @@
+const db = require('../../data/db-config');
+
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('Cars')
+
 }
 
 const getById = () => {
   // DO YOUR MAGIC
+  return db('Cars').where('id', id).first()
+
 }
 
 const create = () => {
   // DO YOUR MAGIC
+  const [id] =  db('Cars').insert(Cars, ['vin', 'make', 'model','mileage', 'title', 'transmission']) // in postgres this would work
+  return create(id)
+
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,44 @@
 // DO YOUR MAGIC
+const express = require('express');
+const db = require('../../data/config-db');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  db('cars')
+    .then(cars => {
+      res.json(cars);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Failed to retrieve fruits' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('cars').where({ id }).first()
+    .then(car => {
+      res.json(car);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'Failed to retrieve fruit' });
+    });
+});
+
+router.post('/', (req, res) => {
+  const carData = req.body;
+  db('Cars').insert(carData)
+    .then(ids => {
+      db('Cars').where({ id: ids[0] })
+        .then(newCar => {
+          res.status(201).json(newCar);
+        });
+    })
+    .catch(err => {
+      console.log('Car error', err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+});
+
+module.exports = router;
